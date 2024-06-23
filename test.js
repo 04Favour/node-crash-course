e353b1d29435841b9de565c91649d69a334c3526
Favour Mfon <favourmichael004@gmail.com>
2024-06-23 04:12:01 +0100

    Add modules

--- test.js
+++ test.js
@@ -1,2 +1,28 @@
-const Name = 'Favour';
-console.log(Name);
+// const Name = 'Favour';
+// console.log(Name);
+
+const fs = require('fs');
+
+const writeFile = fs.writeFile('./os/os.js', "const os = require('os');\nconsole.log(os.platform(), os.freemem())",(err)=> {
+    if (err){
+        console.log(err)
+    }
+    console.log('completed!')
+})
+
+
+if (!fs.existsSync('./os')){
+fs.mkdir('./os', (err)=> {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Folder created!')
+})
+}
+
+fs.readFile('./os/os.js', (err,data)=> {
+    if (err){
+        console.log(err)
+    }
+    console.log(data.toString())
+})
